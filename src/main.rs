312d90c9f6291b3bfe4849dd71b51a0ddea8d135
bananu7 <bananu7@o2.pl>
2022-10-13 15:16:07 +0200

    Reimplemented the mute transaction with new packets - working!

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::net::TcpStream;
 
 pub mod header;
 pub mod packet;
+use packet::*;
 
 fn show(bs: &[u8]) -> String {
     let mut visible = String::new();
@@ -23,17 +24,90 @@ fn read(stream: &mut TcpStream) -> std::io::Result<()> {
         return Ok(());
     }*/
 
-    //println!("{}", show(&buffer));
     println!("{}", nbytes);
+    //println!("{}", show(&buffer));
 
     return Ok(());
 }
 
 fn main() -> std::io::Result<()> {
+    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:55935") {
+        println!("Connected to the server!");
+
+        let mut write = |p: &UcPacket| {
+            write_packet(&p, &mut stream)
+        };
+
+        /*
+        // header - 1
+        write_packet(&UcPacket::UM([0x00,0x00,0x65,0x00,0x5e,0xf1]), &mut stream)?;
+
+        let sub_msg = r#"{"id": "Subscribe","clientName": "","clientInternalName": "","clientType": "PC","clientDescription": "DESKTOP","clientIdentifier": "DESKTOP","clientOptions": "","clientEncoding": 23117}"#;        
+        write_packet(&UcPacket::JM(
+            AddressPair{ a: 0x6a, b: 0x65 },
+            sub_msg.to_string()),
+        &mut stream)?;
+        // Subscription reply (709)
+        read(&mut stream)?;
+
+        write_packet(&UcPacket::UM([0x00,0x00,0x67,0x00,0x5e,0xf1]), &mut stream)?;
+
+        write_packet(&UcPacket::JM(
+            AddressPair{ a: 0x66, b: 0x67 },
+            sub_msg.to_string()),
+        &mut stream)?;
+        // Subscription reply (709)
+        read(&mut stream)?;
+
+        write_packet(&UcPacket::KA(AddressPair{ a: 0x66, b: 0x67 }), &mut stream)?;
+
+        let invoke_method = r#"{"id": "InvokeMethod","url": "","method": "getOptions","callid": 100}"#;
+        write_packet(&UcPacket::JM(
+            AddressPair{ a: 0x66, b: 0x67 },
+            invoke_method.to_string()),
+        &mut stream)?;
+
+        write_packet(&UcPacket::KA(AddressPair{ a: 0x6a, b: 0x65 }), &mut stream)?;
+        */
+        // end of header 1
+
+        // binary_1
+        //write_packet(&UcPacket::KA(AddressPair{ a: 0x68, b: 0x67 }), &mut stream)?;
+        //write_packet(&UcPacket::KA(AddressPair{ a: 0x6a, b: 0x65 }), &mut stream)?;
+
+        // subscribe_2
+        write(&UcPacket::UM([0x00,0x00,0x66,0x00,0x84,0xe8]))?;
+        let sub_msg_uc = r#"{"id": "Subscribe","clientName": "Universal Control","clientInternalName": "ucapp","clientType": "PC","clientDescription": "DESKTOP","clientIdentifier": "DESKTOP","clientOptions": "perm users","clientEncoding": 23117}"#;
+        write(&UcPacket::JM(
+            AddressPair{ a: 0x6b, b: 0x66 },
+            sub_msg_uc.to_string()))?;
+        // end of subscribe_2
+
+        write(&UcPacket::PV(
+            AddressPair{ a: 0x6b, b: 0x66 },
+            "line/ch1/mute".to_string(),
+            true))?;
+
+        write(&UcPacket::FR(
+            AddressPair{ a: 0x6b, b: 0x66 },
+            1,
+            "Listpresets/channel".to_string()))?;
+        // 32 byte response
+        read(&mut stream)?;
+
+    } else {
+        println!("Couldn't connect to server...");
+    }
+
+    Ok(())
+}
+
+fn main3() -> std::io::Result<()> {
    
     if let Ok(mut stream) = TcpStream::connect("127.0.0.1:55935") {
         println!("Connected to the server!");
 
+        /*
         // Subscription request (539)
         stream.write(&header::HEADER)?;
         // Subscription reply (709)
@@ -50,6 +124,8 @@ fn main() -> std::io::Result<()> {
         stream.write(&header::BINARY_1)?;
         // no response
 
+        */
+
         // another subscribe request (282)
         println!("Subsribe_2 ({})", &header::SUBSCRIBE_2.len());
         stream.write(&header::SUBSCRIBE_2)?;
@@ -60,11 +136,11 @@ fn main() -> std::io::Result<()> {
         // two responses(1671, 1708)
 
         // then binary_1 again
-        stream.write(&header::BINARY_1)?;
+        //stream.write(&header::BINARY_1)?;
         // no response
 
         // then binary_2 (12 bytes)
-        stream.write(&header::BINARY_2)?;
+        //stream.write(&header::BINARY_2)?;
         // 12-byte response
         read(&mut stream)?;
 
