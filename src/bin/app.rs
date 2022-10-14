7bde809b8d701d07d524ec2723a249ff623e609d
bananu7 <bananu7@o2.pl>
2022-10-15 00:55:35 +0200

    Rewrote the driver to use MPSC to enable separate heartbeat task

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,4 +1,5 @@
 use ultimate_control::async_driver::AsyncUcDriver;
+use tokio::time::{sleep, Duration};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -7,6 +8,14 @@ async fn main() -> std::io::Result<()> {
     println!("Connected to the server!");
     driver.subscribe().await?;
     driver.ch1_mute(true).await?;
+
+    let mut vol = 0.0f32;
+    for _n in 1..10 {
+        vol += 0.1;
+        driver.ch1_volume(vol).await?;
+        sleep(Duration::from_millis(100)).await;
+    }
+
     driver.read_response().await;
         
     Ok(())
